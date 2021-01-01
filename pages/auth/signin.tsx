c20fd9c39811c97e4b8842a3aa51bb11f3b54576
john <jhezell@go.olemiss.edu>
2021-01-01 12:28:13 -0600

    signin.tsx updated to fit new theme

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,8 +1,9 @@
 import TextInput from "../../components/forms/TextInput";
 import React, {useState} from "react";
-import Button from "../../components/forms/Button";
 import fb from "../../util/firebase-config";
 import {useRouter} from "next/router";
+import {MuiThemeProvider, TextField, Button} from "@material-ui/core";
+import Theme from "../../styles/MuiTheme";
 
 const SignIn = () => {
     const router = useRouter()
@@ -19,7 +20,7 @@ const SignIn = () => {
         toggleLoading(true)
 
         fb.auth().signInWithEmailAndPassword(email, password)
-            .then(() => router.push('/private-page'))
+            .then(() => router.push('/dashboard'))
             .catch((error) => {
                 setError(error.message)
                 toggleLoading(false)
@@ -28,25 +29,26 @@ const SignIn = () => {
     }
 
     return <div className="flex justify-center items-center h-screen">
-        <div>
+        <MuiThemeProvider theme={Theme}>
             <div className="w-96 text-center rounded-lg border border-gray-200 p-8">
                 <form onSubmit={signIn} className="space-y-4">
                     <h1 className="text-4xl font-bold">Log in</h1>
-                    <TextInput label="Email" id="email" required error={error.length > 0} disabled={loading}/>
-                    <TextInput label="Password" type="password" id="password" required error={error.length > 0}
-                               disabled={loading}
-                               helperText={error}/>
-                               <p className="text-md text-left font-semibold text-green-500">{status && "Check your email for a link to reset your password."}</p>
-                    <div className="text-right pt-4">
-                        <Button variant="light"
-                                onClick={() => router.push('/auth/forgot')}
-                                className="mr-2" sizes="lg" color="primary">Forgot password</Button>
-                        <Button variant="filled" sizes="lg" color="primary" type="submit"
-                                disabled={loading} loading={loading}>Log in</Button>
+                    <div className="space-y-2">
+                        <TextField className="w-full" variant="outlined" label="Email" id="email" required error={error.length > 0} disabled={loading}/>
+                        <TextField className="w-full" variant="outlined" label="Password" type="password" id="password" required error={error.length > 0}
+                                   disabled={loading}
+                                   helperText={error}/>
+                        <p className="text-md text-left font-semibold text-green-500">{status && "Check your email for a link to reset your password."}</p>
+                    </div>
+
+                    <div className="text-right pt-4 space-x-2">
+                        <Button disableElevation onClick={() => router.push('/auth/forgot')}
+                                className="mr-2">Forgot password</Button>
+                        <Button disableElevation color="primary" variant="contained" type="submit">Log in</Button>
                     </div>
                 </form>
             </div>
-        </div>
+        </MuiThemeProvider>
     </div>
 
 }
